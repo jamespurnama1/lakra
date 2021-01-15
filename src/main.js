51f81ff46e9105ca9af30c10822c94a91d5f26ef
jamespurnama1 <james.henry.purnama0@gmail.com>
2021-01-16 03:01:55 +0700

    progress bar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import VueNumeric from 'vue-numeric';
+import VueProgressBar from 'vue-progressbar';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -8,14 +9,21 @@ import store from './store';
 
 import 'line-awesome/dist/line-awesome/css/line-awesome.css';
 
+const options = {
+  color: '#80876f',
+  failedColor: '#c74a4a',
+  thickness: '5px',
+};
+
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyCVnPos21-glyEhbMuR1_Bz5rWK69X2pn0',
     libraries: 'places',
   },
   installComponents: true,
-});
-Vue.use(VueNumeric);
+})
+  .use(VueNumeric)
+  .use(VueProgressBar, options);
 
 Vue.config.productionTip = false;
 
