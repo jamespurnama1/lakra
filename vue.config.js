5410589bd8d52eda5ceeba2c2b927ec44ec8874d
jamespurnama1 <james.henry.purnama0@gmail.com>
2021-01-21 04:57:42 +0700

    improved PWA & sitemap

---
+++ vue.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+module.exports = {
+  pwa: {
+    name: 'Lakra',
+    themeColor: '#80876f',
+    msTileColor: '#80876f',
+    appleMobileWebAppCapable: 'yes',
+    assetsVersion: '',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-180x180.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+    workboxOptions: {
+      cacheId: 'lakrasamana',
+      importWorkboxFrom: 'local',
+      navigateFallback: 'shell.html',
+      navigateFallbackWhitelist: [/^((?!\/404).)*$/],
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: 'img/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    configureWebpack: (config) => {
+      if (process.env.NODE_ENV !== 'production') {
+        return {};
+      }
+      return {
+        plugins: [
+          new PrerenderSPAPlugin({
+            staticDir: config.output.path,
+            routes: ['/', '/404', '/projects', '/kpr', '/tentang', '/kontak'],
+            renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
+              renderAfterDocumentEvent: 'rendered',
+            }),
+            // … other Prerender SPA Plugin options …
+          }),
+        ],
+      };
+    },
+  },
+};
